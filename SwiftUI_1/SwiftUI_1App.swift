9150d8112bff75feaed064d58f6f6581da4cca8b
Ravikumar <ravikumar66e@gmail.com>
2023-02-13 21:28:26 +0530

    Initial Commit

---
+++ SwiftUI_1/SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_1App.swift
+//  SwiftUI_1
+//
+//  Created by Apple on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
